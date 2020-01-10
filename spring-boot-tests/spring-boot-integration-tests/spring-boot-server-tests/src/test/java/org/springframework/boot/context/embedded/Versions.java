dfbda9cbb24df3cfa8da042baa1082d7c91af3f7
dipu-726 <51849087+dipu-726@users.noreply.github.com>
2020-01-10 16:27:22 +0530

    Update Versions.java
    Added javaDoc Tags
      - added comments within the source code.
      - changes will be added in final documentation.

--- spring-boot-tests/spring-boot-integration-tests/spring-boot-server-tests/src/test/java/org/springframework/boot/context/embedded/Versions.java
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-server-tests/src/test/java/org/springframework/boot/context/embedded/Versions.java
@@ -37,6 +37,11 @@ final class Versions {
 
 	private Versions() {
 	}
+/**
+  * Returns the version of the Spring boot
+  *
+  * @return returns a String
+  */
 
 	static String getBootVersion() {
 		String baseDir = StringUtils.cleanPath(new File(".").getAbsolutePath());
@@ -44,7 +49,14 @@ final class Versions {
 		mainBaseDir = mainBaseDir.replace("${basedir}", baseDir);
 		return evaluateExpression(mainBaseDir + "/pom.xml", PROPERTIES + "/*[local-name()='revision']/text()");
 	}
-
+	
+  /**
+  * evaluates the Expressions
+  * @param file - name of the file
+  * @param expression - regular expression
+  * @return returns a String
+  */
+	
 	private static String evaluateExpression(String file, String expression) {
 		try {
 			InputSource source = new InputSource(new FileReader(file));
